c072370294cb3249f43f345c649e986eb0074150
Nathan <NParsons@student.neumont.edu>
2018-08-27 10:50:28 -0600

    Finished the main screen and trending fragment with some default images.

--- FOODis_Design/app/src/main/java/com/example/mitch/foodish_design/MainActivity.java
+++ FOODis_Design/app/src/main/java/com/example/mitch/foodish_design/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.mitch.foodish_design;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity implements TrendingFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 10; i++) {
+            TrendingFragment tf = new TrendingFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.trending_list, tf).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
